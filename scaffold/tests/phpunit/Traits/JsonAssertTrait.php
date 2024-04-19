292bb252eac79b9574d1d9103eb33186a4415bb6
Alex Skrypnyk <alex@drevops.com>
2024-04-20 09:12:52 +1000

    Fixed more coding standards.

--- scaffold/tests/phpunit/Traits/JsonAssertTrait.php
+++ scaffold/tests/phpunit/Traits/JsonAssertTrait.php
@@ -6,17 +6,19 @@ use Flow\JSONPath\JSONPath;
 use Helmich\JsonAssert\JsonAssertions;
 
 /**
+ * Trait JsonAssertTrait.
  *
+ * This trait provides a method to assert JSON data.
  */
 trait JsonAssertTrait {
 
   use JsonAssertions;
 
-  public function assertJsonHasNoKey($jsonDocument, string $jsonPath, $message = NULL): void {
-    $result = (new JSONPath($jsonDocument))->find($jsonPath);
+  public function assertJsonHasNoKey(array $json_data, string $path, ?string $message = NULL): void {
+    $result = (new JSONPath($json_data))->find($path);
 
     if (isset($result[0])) {
-      $this->fail($message ?: sprintf("The JSON path '%s' exists, but it was expected not to.", $jsonPath));
+      $this->fail($message ?: sprintf("The JSON path '%s' exists, but it was expected not to.", $path));
     }
 
     $this->addToAssertionCount(1);
